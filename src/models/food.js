beb49e172501623715c155f061907728e9386097
abu-al3ees <malkhazali83@gmail.com>
2021-05-10 02:09:12 +0300

    ..

--- src/models/food.js
+++ src/models/food.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const foodSchema = new mongoose.Schema({
   name: { type: String, required:true },
-  catugary: { type: String },
+  catugary: { type: String, required:false },
 });
 const foodModel = mongoose.model('food', foodSchema);
 
